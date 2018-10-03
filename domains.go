ba7b500188bf050e358adf8b7e618c018afb2a87
Simon Porter <simonporter007@users.noreply.github.com>
2018-10-03 21:35:48 +0100

    Split domains to separate file

---
+++ domains.go
@@ -0,0 +1,24 @@
+package pantheon
+
+var domains = []string{
+	"life",
+	"death",
+	"the sun",
+	"the moon",
+	"nature",
+	"hunting",
+	"war",
+	"balance",
+	"chaos",
+	"law",
+	"protection",
+	"nobility",
+	"luck",
+	"strength",
+	"travel",
+	"destruction",
+	"darkness",
+	"trickery",
+	"light",
+	"knowledge",
+}
